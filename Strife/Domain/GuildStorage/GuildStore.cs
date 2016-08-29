2b034b63fde58ca220a2e6d13ee93c68c9e4a187
George Antonious <antoniou@ualberta.ca>
2016-08-29 13:04:30 -0600

    Added GuildStore

---
+++ Strife/Domain/GuildStorage/GuildStore.cs
@@ -0,0 +1,25 @@
+﻿using Discord_UWP.API.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord_UWP.SharedModels;
+
+namespace Strife.Domain.GuildStorage
+{
+    public class GuildStore : IGuildStore
+    {
+        private readonly IUserService _userService;
+
+        public GuildStore(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<IEnumerable<UserGuild>> GetGuildsAsync()
+        {
+            return await _userService.GetCurrentUserGuilds();
+        }
+    }
+}
---
+++ Strife/Domain/GuildStorage/IGuildStore.cs
@@ -0,0 +1,14 @@
+﻿using Discord_UWP.SharedModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strife.Domain.GuildStorage
+{
+    public interface IGuildStore
+    {
+        Task<IEnumerable<UserGuild>> GetGuildsAsync();
+    }
+}
