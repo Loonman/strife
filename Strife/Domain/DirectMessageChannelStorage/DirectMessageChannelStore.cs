73fe371f99f59ea8049f475de74aa529921e11c9
George Antonious <antoniou@ualberta.ca>
2016-08-29 13:09:54 -0600

    Added DirectMessageChannel store

---
+++ Strife/Domain/DirectMessageChannelStorage/DirectMessageChannelStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord_UWP.SharedModels;
+using Discord_UWP.API.User;
+
+namespace Strife.Domain.DirectMessageChannelStorage
+{
+    public class DirectMessageChannelStore : IDirectMessageChannelStore
+    {
+        private readonly IUserService _userService;
+
+        public async Task<IEnumerable<DirectMessageChannel>> GetDirectMessageChannelsAsync()
+        {
+            return await _userService.GetCurrentUserDirectMessageChannels();
+        }
+    }
+}
---
+++ Strife/Domain/DirectMessageChannelStorage/IDirectMessageChannelStore.cs
@@ -0,0 +1,14 @@
+﻿using Discord_UWP.SharedModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strife.Domain.DirectMessageChannelStorage
+{
+    public interface IDirectMessageChannelStore
+    {
+        Task<IEnumerable<DirectMessageChannel>> GetDirectMessageChannelsAsync();
+    }
+}
