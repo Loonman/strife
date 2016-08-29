aef5edb443f447e17c11ae77435e11da20bf05e2
George Antonious <antoniou@ualberta.ca>
2016-08-29 14:59:34 -0600

    Build all the dependencies in the MainPage.cs, hopfully will have DI soon

--- Strife/MainPage.xaml.cs
+++ Strife/MainPage.xaml.cs
@@ -19,6 +19,11 @@ using Discord_UWP.Authentication;
 using Discord_UWP.Gateway;
 using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
+using Strife.ViewModels;
+using Strife.Domain;
+using Strife.Domain.MessageStorage;
+using Strife.Domain.GuildChannelStorage;
+using Strife.Domain.GuildStorage;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -29,8 +34,50 @@ namespace Strife
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public GuildsViewModel GuildsViewModel { get; set; }
+        public ChannelsViewModel ChannelsViewModel { get; set; }
+        public MessagesViewModel MessagesViewModel { get; set; }
+
         public MainPage()
         {
+            InitAsync();            
+        }
+
+        public async void InitAsync()
+        {
+            var apiConfig = new DiscordApiConfiguration
+            {
+                BaseUrl = "https://discordapp.com/api"
+            };
+
+            var gatewayConfig = new GatewayConfig
+            {
+                BaseUrl = "wss://gateway.discord.gg/"
+            };
+
+            var authenticator = new DiscordAuthenticator("PUT TOKEN HERE");
+            var authenticatedRestFactory = new AuthenticatedRestFactory(apiConfig, authenticator);
+
+            var channelService = authenticatedRestFactory.GetChannelService();
+            var guildService = authenticatedRestFactory.GetGuildService();
+            var userService = authenticatedRestFactory.GetUserService();
+
+            var gateway = new Gateway(gatewayConfig, authenticator);
+
+            await gateway.ConnectAsync();
+
+            var messageStore = new MessageStore(channelService, gateway);
+            var messageProvider = new MessageProvider(messageStore, "81384956881809408");
+
+            var channelStore = new GuildChannelStore(guildService, gateway);
+            var channelProvider = new GuildChannelProvider(channelStore, "81384788765712384");
+
+            var guildStore = new GuildStore(userService);
+
+            MessagesViewModel = new MessagesViewModel(messageProvider);
+            ChannelsViewModel = new ChannelsViewModel(channelProvider);
+            GuildsViewModel = new GuildsViewModel(guildStore);
+
             this.InitializeComponent();
         }
     }
---
+++ Strife/Utils/DictionaryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strife.Utils
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+        {
+            if (key == null || !dictionary.ContainsKey(key))
+            {
+                return defaultValue;
+            }
+            return dictionary[key];
+        }
+    }
+}
